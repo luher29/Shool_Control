a0e633de9297c1e2a1ee6822ea5c641a922cb46d
Lupita <luher2912@gmail.com>
2025-11-18 17:17:20 -0600

    first commit 2

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
-//Aqui se pone la cadena de conección y se inicia la conección con mongo
-import { Console } from "console";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://JesusCruz:Jesuscruz5@ingjesus.gkiv4fg.mongodb.net/school_control?retryWrites=true&w=majority&appName=IngJesus")
-    .then(() => console.log("Mongodb conected"))
-    .catch((err) => console.error(err));
+mongoose.connect("mongodb+srv://lupita29:luher29@lupita.vup0axr.mongodb.net/school_control?retryWrites=true&w=majority&appName=Lupita")
+//then para responder que si hubo connecion
+.then(()=>console.log("mongo db connected"))
+//catch si hubo un herror 
+.catch((err)=>console.log(err));
 
 export default mongoose;
